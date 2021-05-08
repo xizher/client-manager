145ed2b2d8afdf79b900f7a7010f3aabe58305f3
xizher <xizher@163.com>
2021-05-08 10:48:25 +0800

    chore: 路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   server: {
     port: 8080
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      '~hooks': path.resolve(__dirname, 'src/hooks'),
+      '~cp': path.resolve(__dirname, 'src/components'),
+      '~services': path.resolve(__dirname, 'src/services'),
+    }
   },
   plugins: [
     createVuePlugin()
