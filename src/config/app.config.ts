0e4d751440aa067878745adf5d3b6dc65a96259a
xizher <xizher@163.com>
2021-05-08 10:56:04 +0800

    feat: 页面标识与系统配置项集成

---
+++ src/config/app.config.ts
@@ -0,0 +1,9 @@
+export const config = {
+  title: '📚信息管理系统📚',
+}
+
+function install () : void {
+  document.title = config.title
+}
+
+export default { install }
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
 import App from './App.vue'
 import router from './router'
+import appConfig from './config/app.config'
 
 Vue.use(VueCompositionAPI)
+Vue.use(appConfig)
 
 new Vue({
   el: '#app',
