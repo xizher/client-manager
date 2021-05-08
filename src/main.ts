bbd0dbecadf74b5ca829d39796e16d3524323bc6
xizher <xizher@163.com>
2021-05-08 10:41:11 +0800

    feat: 路由集成

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueCompositionAPI from '@vue/composition-api'
 import App from './App.vue'
+import router from './router'
 
 Vue.use(VueCompositionAPI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router,
 })
---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import VueRouter, { RouteConfig } from 'vue-router'
+
+const routes: RouteConfig[] = [
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'hash',
+})
+
+export default router
