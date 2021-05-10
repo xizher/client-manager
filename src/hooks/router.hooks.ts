71dc33bd0a7bc05e6a58b9efcde793c3b8c0cf15
xizher <xizher@163.com>
2021-05-10 09:15:04 +0800

    fix: 缓存 $router对象

--- src/hooks/router.hooks.ts
+++ src/hooks/router.hooks.ts
@@ -1,11 +1,16 @@
 import { getCurrentInstance } from '@vue/composition-api'
+import VueRouter from 'vue-router'
 
 interface IRouterHook {
   push (path: string) : void
 }
 
+let router: VueRouter | undefined
+
 export function useRouter () : IRouterHook {
-  const router = getCurrentInstance()?.proxy.$router
+  if (!router) {
+    router = getCurrentInstance()?.proxy.$router
+  }
   return {
     push (path: string) {
       router?.push(path)
