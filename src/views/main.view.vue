d1a8c7aa15f8ced2b093d5c90b3368259456fb78
xizher <xizher@163.com>
2021-05-11 09:34:30 +0800

    fix: 解决在移动端中使用100vh高度的显示问题

--- src/views/main.view.vue
+++ src/views/main.view.vue
@@ -6,30 +6,36 @@
 </template>
 
 <script>
-import { defineComponent, ref, watchEffect } from '@vue/composition-api'
-import { config } from '~/config/app.config'
-import { useRouter } from '~/hooks/router.hooks'
+import { defineComponent, onMounted, onUnmounted } from '@vue/composition-api'
 
 export default defineComponent({
   components: {
     BottomNav: () => import('~/components/BottomNav.vue')
   },
   setup () {
-    const { navConfig } = config
-    const navItems = navConfig.items
-    const selectedKey = ref(navConfig.defaultPath)
-    const router = useRouter()
-    watchEffect(() => router.push(selectedKey.value))
-    return {
-      navItems, selectedKey
+
+    function handleResize () {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
     }
+
+    onMounted(() => {
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+      window.addEventListener('resize', handleResize)
+    })
+    onUnmounted(() => {
+      window.removeEventListener('resize', handleResize)
+    })
   }
 })
 </script>
 
 <style lang="scss" scoped>
 .view-main {
-  height: 100vh;
+  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: calc(var(--vh, 1vh) * 100);
   display: flex;
   flex-direction: column;
   .content {
